ce99351bc1a2f373d55c527033e7ae9dcaa37e24
ssjiyeong <sssjiyeong@gmail.com>
2025-11-13 21:15:24 +0900

    Run CELLIA + Web

--- run_cellia_web.py
+++ run_cellia_web.py
@@ -114,7 +114,7 @@ def main():
         port=args.port,
         debug=True,
         rationale_json_path=args.rationale_json,
-        num_top_k=args.num_top_k
+        num_top_k=args.n_top_markers
     )
 
 
