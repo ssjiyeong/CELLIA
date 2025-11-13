e0776a5a3d6f1b969df11c94251ab966c313d940
ssjiyeong <sssjiyeong@gmail.com>
2025-11-13 18:52:57 +0900

    Run CELLIA Web

---
+++ run_cellia_web.py
@@ -0,0 +1,44 @@
+# run_cellia_web.py
+
+import os
+import scanpy as sc
+from cellia import *
+from cellia_web import *
+
+
+def main():
+    print("Running CELLIA...")
+
+    # Load input data
+    adata = sc.read_h5ad("dataset/CRC.h5ad")
+
+    # Run processing pipeline
+    result = cellia_run(
+        adata=adata,
+        tissue_db="crc|colon",
+        tissue_type="human colorectal cancer (CRC)",
+        api_key=os.getenv("API_KEY"),
+        n_top_markers=15
+    )
+
+    adata.write_h5ad("dataset/CRC_cellia_annotated.h5ad")
+
+    print(result)
+
+    print("CELLIA annotation completed successfully.")
+    print("Save to dataset/CRC_cellia_annotated.h5ad")
+
+    rationale_path = "../cellia_output/gpt_explanations_db.json"
+
+    # 7) 웹 앱 실행
+    launch_cap_style_app(
+        adata=adata,
+        port=8051,
+        debug=True,
+        rationale_json_path=rationale_path,
+    )
+
+
+
+if __name__ == "__main__":
+    main()
